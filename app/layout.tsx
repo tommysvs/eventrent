c82ac003dacf1adfcaa1c86837446a613d97eef0
Tommy Vega <tommysvs@hotmail.com>
2026-07-23 14:56:22 -0600

    changed logo icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,19 +15,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
+        url: '/logo.png',
+        type: 'image/png',
+        sizes: '32x32',
       },
     ],
-    apple: '/apple-icon.png',
   },
 }
 
